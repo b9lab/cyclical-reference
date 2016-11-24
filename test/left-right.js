2e66a8d04de0974eaaaae1cefce5a096b4825d2d
Xavier Lepretre <xavier@b9lab.com>
2016-11-24 22:11:10 +0000

    Made test own deployed contracts.

--- test/left-right.js
+++ test/left-right.js
@@ -1,17 +1,52 @@
+ethJsUtil = require('../node_modules/ethereumjs-util/');
+Extensions = require("../utils/extensions.js");
+Extensions.init(web3, assert);
+
 contract('Left and Right', function(accounts) {
+    var owner;
+    var left;
+    var right;
 
-    it("should have each other's address", function() {
-        var left = Left.deployed();
-        var right = Right.deployed();
+    before("should prepare accounts", function () {
+        assert.isAbove(accounts.length, 1, "should have at least 1 account");
+        owner = accounts[0];
+        return Extensions.makeSureAreUnlocked([ owner ]);
+    });
 
-        return left.right()
-            .then(function(rightAddress) {
-                assert.equal(rightAddress, right.address, "should have been the same right address");
-                return right.left();
-            })
-            .then(function(leftAddress) {
-                assert.equal(leftAddress, left.address, "should have been the same left address");
-            });
+    describe("Cyclical", function() {
+
+        beforeEach("should deploy", function() {
+            var currentNonce = web3.eth.getTransactionCount(web3.eth.accounts[0]);
+            var futureLeftNonce = currentNonce;
+            var futureLeftAddress = ethJsUtil.bufferToHex(ethJsUtil.generateAddress(
+                web3.eth.accounts[0], futureLeftNonce));
+            var futureRightNonce = futureLeftNonce + 1;
+            var futureRightAddress = ethJsUtil.bufferToHex(ethJsUtil.generateAddress(
+                web3.eth.accounts[0], futureRightNonce));
+
+            return Promise.all([ 
+                    Left.new(futureRightAddress),
+                    Right.new(futureLeftAddress)
+                ])
+                .then(function(createds) {
+                    left = createds[0];
+                    right = createds[1];
+                });
+
+        });
+
+        it("should have each other's address", function() {
+
+            return left.right()
+                .then(function(rightAddress) {
+                    assert.equal(rightAddress, right.address, "should have been the same right address");
+                    return right.left();
+                })
+                .then(function(leftAddress) {
+                    assert.equal(leftAddress, left.address, "should have been the same left address");
+                });
+        });
+
     });
 
 });
---
+++ utils/extensions.js
@@ -0,0 +1,174 @@
+module.exports = {
+    init: function (web3, assert) {
+        web3.eth.getTransactionReceiptMined = function (txnHash, interval) {
+            var transactionReceiptAsync;
+            interval = interval ? interval : 500;
+            transactionReceiptAsync = function(txnHash, resolve, reject) {
+                try {
+                    var receipt = web3.eth.getTransactionReceipt(txnHash);
+                    if (receipt == null) {
+                        setTimeout(function () {
+                            transactionReceiptAsync(txnHash, resolve, reject);
+                        }, interval);
+                    } else {
+                        resolve(receipt);
+                    }
+                } catch(e) {
+                    reject(e);
+                }
+            };
+
+            if (Array.isArray(txnHash)) {
+                var promises = [];
+                txnHash.forEach(function (oneTxHash) {
+                    promises.push(web3.eth.getTransactionReceiptMined(oneTxHash, interval));
+                });
+                return Promise.all(promises);
+            } else {
+                return new Promise(function (resolve, reject) {
+                        transactionReceiptAsync(txnHash, resolve, reject);
+                    });
+            }
+        };
+
+        web3.eth.signPromise = function (account, dataToSign) {
+            return new Promise(function (resolve, reject) {
+                web3.eth.sign(account, dataToSign, function (error, value) {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(value);
+                    }
+                });
+            });
+        };
+
+        assert.isTxHash = function (txnHash, message) {
+            assert(typeof txnHash === "string",
+                'expected #{txnHash} to be a string',
+                'expected #{txnHash} to not be a string');
+            assert(txnHash.length === 66,
+                'expected #{txnHash} to be a 66 character transaction hash (0x...)',
+                'expected #{txnHash} to not be a 66 character transaction hash (0x...)');
+
+            // Convert txnHash to a number. Make sure it's not zero.
+            // Controversial: Technically there is that edge case where
+            // all zeroes could be a valid address. But: This catches all
+            // those cases where Ethereum returns 0x0000... if something fails.
+            var number = web3.toBigNumber(txnHash, 16);
+            assert(number.equals(0) === false, 
+                'expected address #{txnHash} to not be zero', 
+                'you shouldn\'t ever see this.');
+        };
+
+    },
+
+    getEventsPromise: function (myFilter, count, timeOut) {
+        timeOut = timeOut ? timeOut : 30000;
+        var promise = new Promise(function (resolve, reject) {
+            count = (typeof count !== "undefined") ? count : 1;
+            var results = [];
+            var toClear = setTimeout(function () {
+                myFilter.stopWatching();
+                reject(new Error("Timed out"));
+            }, timeOut);
+            myFilter.watch(function (error, result) {
+                if (error) {
+                    clearTimeout(toClear);
+                    reject(error);
+                } else {
+                    count--;
+                    results.push(result);
+                }
+                if (count <= 0) {
+                    clearTimeout(toClear);
+                    myFilter.stopWatching();
+                    resolve(results);
+                }
+            });
+        });
+        if (count == 0) {
+            promise = promise
+                .then(function (events) {
+                    throw "Expected to have no event";
+                })
+                .catch(function (error) {
+                    if (error.message != "Timed out") {
+                        throw error;
+                    }
+                });
+        }
+        return promise;
+    },
+    
+    expectedExceptionPromise: function (action, gasToUse, timeOut) {
+        timeOut = timeOut ? timeOut : 5000;
+        var promise = new Promise(function (resolve, reject) {
+                try {
+                    resolve(action());
+                } catch(e) {
+                    reject(e);
+                }
+            })
+            .then(function (txnHash) {
+                assert.isTxHash(txnHash, "it should have thrown");
+                return web3.eth.getTransactionReceiptMined(txnHash);
+            })
+            .then(function (receipt) {
+                // We are in Geth
+                assert.equal(receipt.gasUsed, gasToUse, "should have used all the gas");
+            })
+            .catch(function (e) {
+                if ((e + "").indexOf("invalid JUMP") > -1 || (e + "").indexOf("out of gas") > -1) {
+                    // We are in TestRPC
+                } else if ((e + "").indexOf("please check your gas amount") > -1) {
+                    // We are in Geth for a deployment
+                } else {
+                    throw e;
+                }
+            });
+
+        return promise;
+    },
+
+    waitPromise: function (timeOut, toPassOn) {
+        timeOut = timeOut ? timeOut : 1000;
+        return new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                    resolve(toPassOn);
+                }, timeOut);
+        });
+    },
+
+    makeSureHasAtLeast: function (richAccount, recipients, wei) {
+        var requests = [];
+        recipients.forEach(function (recipient) {
+            if (web3.eth.getBalance(recipient).lessThan(wei)) {
+                requests.push(web3.eth.sendTransaction({
+                    from: richAccount,
+                    to: recipient,
+                    value: web3.toWei(2)
+                }));
+            }
+        });
+        return requests;
+    },
+
+    makeSureAreUnlocked: function (accounts) {
+        var requests = [];
+        accounts.forEach(function (account, index) {
+            requests.push(web3.eth.signPromise(
+                    account,
+                    "0x0000000000000000000000000000000000000000000000000000000000000000")
+                .catch(function (error) {
+                    if (error.message == "account is locked") {
+                        throw Error("account " + account + " at index " + index + " is locked");
+                    } else {
+                        throw error;
+                    }
+                }));
+        });
+        return Promise.all(requests);
+    }
+
+};
